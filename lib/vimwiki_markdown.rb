5cb1ffdf4a532d869b975816a594ed21fc44f5bc
Patrick Davey <patrick.davey@gmail.com>
2014-10-14 16:41:52 +1300

    Setting enforce_available_locales to false for deprecation

--- lib/vimwiki_markdown.rb
+++ lib/vimwiki_markdown.rb
@@ -2,6 +2,8 @@ require "vimwiki_markdown/version"
 require "vimwiki_markdown/options"
 require "vimwiki_markdown/template"
 require "vimwiki_markdown/wiki_body"
+
 module VimwikiMarkdown
   # Your code goes here...
+  ::I18n.enforce_available_locales = false
 end
