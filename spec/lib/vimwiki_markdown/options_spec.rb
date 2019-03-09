a3e3e45701beb8dfd9b90cb33061d70e77c10e55
Patrick Davey <patrick.davey@gmail.com>
2019-03-09 19:39:57 +1300

    Add basic covering spec for extension

--- spec/lib/vimwiki_markdown/options_spec.rb
+++ spec/lib/vimwiki_markdown/options_spec.rb
@@ -14,6 +14,25 @@ module VimwikiMarkdown
       its(:syntax) { should eq('markdown') }
       its(:output_fullpath) { should eq("#{subject.output_dir}#{subject.title.parameterize}.html") }
       its(:template_filename) { should eq('~/vimwiki/templates/default.tpl') }
+
+      describe "extension" do
+        it "deals with a different wiki extension correctly" do
+          allow(Options).to receive(:arguments).and_return(
+            ["1", #force - 1/0
+             "markdown",
+             "wiki",
+             "~/vimwiki/site_html/",
+             "~/vimwiki/index.wiki",
+             "~/vimwiki/site_html/style.css",
+             "~/vimwiki/templates/",
+             "default",
+             ".tpl",
+             "-"]
+          )
+
+          expect(Options.new.title).to eq("Index")
+        end
+      end
     end
   end
 end
